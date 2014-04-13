ddae13c3236e882e976c0ca845757a3ce906540c
Rob H <oeoeaio@gmail.com>
2014-04-13 16:46:59 +1000

    Move CMS admin to /cms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  comfy_route :cms_admin, :path => '/admin'
+  comfy_route :cms_admin, :path => '/cms'
 
   # Make sure this routeset is defined last
   comfy_route :cms, :path => '/', :sitemap => false
