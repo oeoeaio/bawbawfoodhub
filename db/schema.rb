34a74b620ff7e2e022e97d3dc28a66a0a555556c
Rob Harrington <oeoeaio@gmail.com>
2014-11-23 18:16:00 +1100

    Adding admin devise model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141123061249) do
+ActiveRecord::Schema.define(version: 20141123070545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: true do |t|
+    t.string   "email",              default: "", null: false
+    t.string   "encrypted_password", default: "", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
 
   create_table "cms_blocks", force: true do |t|
     t.integer  "page_id",    null: false
