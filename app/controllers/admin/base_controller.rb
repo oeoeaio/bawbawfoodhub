2bb8ce4bbae4cd42286de30512d50fabc3bce06f
Rob Harrington <oeoeaio@gmail.com>
2014-11-23 23:59:04 +1100

    Can create new seasons

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -5,4 +5,11 @@ class Admin::BaseController < ApplicationController
     # Send the currently logged in admin user to pundit for admin actions
     current_admin
   end
+
+  private
+
+  # Method to work out the action name for pundit to authorize for admin
+  def authorize_admin(resource = controller_name.classify.constantize)
+    authorize resource, "admin_#{action_name}?".to_sym
+  end
 end
--- app/controllers/admin/seasons_controller.rb
+++ app/controllers/admin/seasons_controller.rb
@@ -1,6 +1,22 @@
 class Admin::SeasonsController < Admin::BaseController
+  before_filter :authorize_admin, only: [:index, :new, :create]
+
   def index
     @seasons = Season.all
-    authorize Season, :admin_index?
+  end
+
+  def new
+    @season = Season.new
+  end
+
+  def create
+    @season = Season.create season_params
+    redirect_to admin_seasons_path
+  end
+
+  private
+
+  def season_params
+    params.require(:season).permit(:name, :slug, :places_remaining, :signups_open)
   end
 end
