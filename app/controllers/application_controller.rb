8be740e8308c72a659239a835450ecb228e59440
Rob Harrington <oeoeaio@gmail.com>
2014-11-23 22:48:42 +1100

    Pundit unauthorised messages gracefully redirect to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from Pundit::NotAuthorizedError, with: :unauthorized
+
   private
   def load_cms_context
       @cms_site = Cms::Site.first
+  end
+
+  def unauthorized
+    flash[:error] = "You are not authorised to access that page."
+    redirect_to(request.referrer || root_path)
   end
 end
