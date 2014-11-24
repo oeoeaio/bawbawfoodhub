0c7ca2d4b77ab2758500b22166021e2b92b791ae
Rob Harrington <oeoeaio@gmail.com>
2014-11-25 09:35:50 +1100

    Make error messages look a bit better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def error_class(target, field)
+    target.errors[field].any? ? "error": ""
+  end
+
   def first_error_for(target, field)
     if target.errors[field].any?
       render 'shared/field_error', target: target, field: field
