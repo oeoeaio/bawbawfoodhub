79985e946354b59ad20e3308424c4a528ffbb4ea
Rob Harrington <oeoeaio@gmail.com>
2014-11-24 15:46:03 +1100

    Adding box sizes to subscription model and view

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,6 +3,11 @@ class Subscription < ActiveRecord::Base
   belongs_to :user
   validates :season, presence: true
   validates :user, presence: true
+  SIZES = {
+    large: 'Large Box',
+    standard: 'Standard Box',
+    small: 'Small Bag'
+  }
 
   accepts_nested_attributes_for :user
 end
