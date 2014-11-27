5d311ea90c852f7b8bd93546ba0354c991431fcd
Rob Harrington <oeoeaio@gmail.com>
2014-11-28 01:21:23 +1100

    Season have many pack days

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,5 +1,6 @@
 class Season < ActiveRecord::Base
   has_many :subscriptions
+  has_many :pack_days
   has_many :users, through: :subscriptions
 
   validates :slug, uniqueness: true, presence: true
