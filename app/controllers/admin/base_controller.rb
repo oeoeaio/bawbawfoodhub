bceadc06d7eb1986f69ffcea2c928c72ffda4c9c
Rob Harrington <oeoeaio@gmail.com>
2015-05-24 14:44:43 +1000

    Adding an admin navigation bar

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,6 @@
 class Admin::BaseController < ApplicationController
   after_action :verify_authorized
+  layout 'admin'
 
   def pundit_user
     # Send the currently logged in admin user to pundit for admin actions
