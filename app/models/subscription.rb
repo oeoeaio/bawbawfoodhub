e9a9d36d9abe594abdbdc0362cfd124d93b4fe9c
Rob Harrington <oeoeaio@gmail.com>
2015-05-24 22:12:53 +1000

    Pulling box sizes hash down into lib module

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,25 +1,5 @@
 class Subscription < ActiveRecord::Base
-  SIZES = {
-    "small" => {
-      name: 'Small Bag',
-      price: '$20',
-      value: 20,
-      desc: 'We offer this smaller package of delicious fresh veggies to cater for the needs of smaller households of one or two people.'
-    },
-    "standard" => {
-      name: 'Standard Box',
-      price: '$30',
-      value: 30,
-      desc: 'Our standard weekly box of vegetables is our most popular, and is packed with fresh seasonal produce for 2-3 people for a week.'
-    },
-    "large" => {
-      name: 'Large Box',
-      price: '$45',
-      value: 45,
-      desc: 'The large version of our weekly box is suitable for bigger households, and contains enough vegetables to feed around 3-4 people for a week.'
-    }
-  }
-
+  include BoxSize
   belongs_to :season
   belongs_to :user
   validate :ensure_season_is_open
