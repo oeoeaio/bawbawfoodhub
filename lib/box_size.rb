f90db4f364742043b447bbe548d5c897f395c2c2
Rob H <oeoeaio@gmail.com>
2015-08-14 18:21:10 -0400

    Changing box prices

--- lib/box_size.rb
+++ lib/box_size.rb
@@ -2,20 +2,20 @@ module BoxSize
   SIZES = {
     "small" => {
       name: 'Small Bag',
-      price: '$20',
-      value: 20,
+      price: '$22',
+      value: 22,
       desc: 'We offer this smaller package of delicious fresh veggies to cater for the needs of smaller households of one or two people.'
     },
     "standard" => {
       name: 'Standard Box',
-      price: '$30',
-      value: 30,
+      price: '$32',
+      value: 32,
       desc: 'Our standard weekly box of vegetables is our most popular, and is packed with fresh seasonal produce for 2-3 people for a week.'
     },
     "large" => {
       name: 'Large Box',
-      price: '$45',
-      value: 45,
+      price: '$47',
+      value: 47,
       desc: 'The large version of our weekly box is suitable for bigger households, and contains enough vegetables to feed around 3-4 people for a week.'
     }
   }
