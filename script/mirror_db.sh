b667a18759607b94353ed036a92907dab9574c48
Rob Harrington <oeoeaio@gmail.com>
2016-02-20 10:52:53 +1100

    Adding script to mirror production db to local dev environment

---
+++ script/mirror_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Usage: script/mirror_db.sh
+
+set -e
+
+DB_HOST='bbfh'
+DB_USER='bbfh'
+DB_DATABASE='bbfh_prod'
+
+# -- Mirror database
+echo "Mirroring database..."
+echo "drop database bbfh_dev" | psql -h localhost -U bbfh bbfh_test
+echo "create database bbfh_dev" | psql -h localhost -U bbfh bbfh_test
+ssh $DB_HOST "pg_dump -h localhost -U $DB_USER $DB_DATABASE |gzip" |gunzip |psql -h localhost -U bbfh bbfh_dev
