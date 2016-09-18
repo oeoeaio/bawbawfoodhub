8cddbe726ac123b34ab32e5b11ea0377c55b8d01
Rob Harrington <oeoeaio@gmail.com>
2016-09-19 09:17:59 +1000

    Showing recent subscriptions on the admin dashboard

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -2,6 +2,7 @@ class Admin::HomeController < Admin::BaseController
 
   def index
     authorize_admin :home
+    @subscriptions = Subscription.where('created_at > ?', 1.month.ago).order('created_at DESC')
   end
 
 end
