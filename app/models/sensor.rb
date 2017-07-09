6ee1f3c6e8b76f3bf4422317ba8b2a5155c684f4
Rob Harrington <oeoeaio@gmail.com>
2017-07-09 15:20:23 +1000

    Updating SensorChecker to use new fail count
    SMS will now only be sent once (n) consecutive failures occur in a row

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -7,4 +7,20 @@ class Sensor < ActiveRecord::Base
   def last_reading
     readings.order(recorded_at: :desc).limit(1).first
   end
+
+  def last_n_readings(n)
+    readings.order(recorded_at: :desc).limit(n).first(n)
+  end
+
+  def fail_count_reached?
+    value_failures_in_last_n_readings == fail_count_for_value_alert
+  end
+
+  private
+
+  def value_failures_in_last_n_readings
+    last_n_readings(fail_count_for_value_alert).count do |reading|
+      !reading.value.between?(lower_limit, upper_limit)
+    end
+  end
 end
