f71476bb4e582734f3ab3122c66df371bb6711f2
Rob Harrington <oeoeaio@gmail.com>
2019-07-30 21:40:07 +1000

    Use global scope to access MailChimp::Api service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def newsletters
-    @campaigns = Mailchimp::Api.get_campaigns["campaigns"].sort_by{ |c| c["send_time"] }.reverse
+    @campaigns = ::Mailchimp::Api.get_campaigns["campaigns"].sort_by{ |c| c["send_time"] }.reverse
   end
 
   def questions
