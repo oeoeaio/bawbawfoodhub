ef181ee4ca648b237e4837f140d72f7bbf527bf9
Rob Harrington <oeoeaio@gmail.com>
2019-09-27 05:51:30 +1000

    Add flash message warning of closure for GF public holiday

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  def index
+    flash[:alert] = "We will be closed for the Grand Final Day public holiday. Friday 27th of September." unless Date.today > Date.new(2019, 9, 27)
+  end
 
   def newsletters
     @campaigns = Mailchimp::Api.get_campaigns["campaigns"].sort_by{ |c| c["send_time"] }.reverse
