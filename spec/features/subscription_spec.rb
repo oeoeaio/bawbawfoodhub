49258f06e0520a3d21123f17ccc9e02f4d5aec27
Rob Harrington <oeoeaio@gmail.com>
2019-10-04 22:15:48 +1000

    Add basic feature specs for subscriptions

---
+++ spec/features/subscription_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe 'Subscriptions', type: :system, js: true do
+  before do
+    create(:season)
+  end
+
+  def enter_user_details
+    fill_in 'subscription_user_attributes_given_name', with: 'Jenny'
+    fill_in 'subscription_user_attributes_surname', with: 'Vale'
+    fill_in 'subscription_user_attributes_email', with: 'jennyvale@gmail.com'
+    fill_in 'subscription_user_attributes_phone', with: '0356223344'
+    fill_in 'subscription_user_attributes_password', with: '12345678'
+    fill_in 'subscription_user_attributes_password_confirmation', with: '12345678'
+  end
+
+  describe 'for weekly pickup' do
+    it 'allows the user to subscribe for a veggie box' do
+      visit root_path
+      click_link 'Sign Up'
+
+      find("label[for='subscription_box_size_standard']").click
+
+      select('Collection from 4/133 North Road, Warragul (Free)', from: 'subscription_delivery')
+      select('Weekly', from: 'subscription_frequency')
+
+      enter_user_details
+
+      click_button 'Complete Sign Up'
+      expect(page).to have_content 'Thanks Jenny!'
+    end
+  end
+
+  context 'for fortnightly delivery' do
+    it 'allows the user to subscribe for a veggie box' do
+      visit root_path
+      click_link 'Sign Up'
+
+      find("label[for='subscription_box_size_standard']").click
+
+      select('Delivery to Warragul or Drouin ($5.00)', from: 'subscription_delivery')
+      select('Fortnightly', from: 'subscription_frequency')
+
+      enter_user_details
+
+      fill_in 'subscription_street_address', with: '123 Alfred St'
+      fill_in 'subscription_town', with: 'Warragul'
+      fill_in 'subscription_postcode', with: '3820'
+
+      click_button 'Complete Sign Up'
+
+      expect(page).to have_content 'Thanks Jenny!'
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,8 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'capybara/rspec'
+require 'selenium/webdriver'
 require 'spec_helper'
 require 'shoulda/matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -25,6 +27,9 @@ require 'shoulda/matchers'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.server = :webrick
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -33,6 +38,10 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.before(:each, type: :system, js: true) do
+		driven_by(:selenium_chrome_headless)
+	end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
