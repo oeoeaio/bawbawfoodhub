8a7149a55cc7152d9e8ab0c228fce73d5062b9c9
Rob H <oeoeaio@gmail.com>
2019-11-09 12:21:14 +1100

    Show trial prices on the signup page

--- lib/box_size.rb
+++ lib/box_size.rb
@@ -1,20 +1,23 @@
 module BoxSize
   SIZES = {
     "small" => {
-      name: 'Small Bag',
+      name: 'Small Box',
       price: '$22',
+      trial_price: '$25',
       value: 22,
       desc: 'We offer this smaller package of delicious fresh veggies to cater for the needs of smaller households of one or two people.'
     },
     "standard" => {
       name: 'Standard Box',
       price: '$32',
+      trial_price: '$35',
       value: 32,
       desc: 'Our standard weekly box of vegetables is our most popular, and is packed with fresh seasonal produce for 2-3 people for a week.'
     },
     "large" => {
       name: 'Large Box',
       price: '$47',
+      trial_price: '$50',
       value: 47,
       desc: 'The large version of our weekly box is suitable for bigger households, and contains enough vegetables to feed around 3-4 people for a week.'
     }
