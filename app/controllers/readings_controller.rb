8ef03d08a8bda41ce7a6661a11908a2d9d84cf9d
Rob H <oeoeaio@gmail.com>
2020-01-11 20:56:11 +1100

    Allow sensors to be identified by id

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,13 +1,12 @@
 class ReadingsController < ApplicationController
   protect_from_forgery except: :create
   before_action :verify_request, only: :create
+  before_action :load_sensor, only: :create
 
   respond_to :json
 
   def create
-    sensor = Sensor.find_by_name(params[:sensor_name])
-    return head :ok unless sensor.present?
-    Reading.create(sensor: sensor, value: params[:value], recorded_at: Time.now)
+    Reading.create(sensor: @sensor, value: params[:value], recorded_at: Time.now)
     head :ok
   end
 
@@ -18,5 +17,12 @@ class ReadingsController < ApplicationController
     digest = Digest::SHA1.hexdigest("#{pass}#{params[:salt]}")
     # Don't run the action unless the hash checks out
     head :ok unless params[:hash] == digest
+  end
+
+  def load_sensor
+    @sensor = Sensor.find_by_identifier(params[:sensor_id]) if params[:sensor_id]
+    @sensor ||= Sensor.find_by_name(params[:sensor_name])
+
+    return head :ok unless @sensor.present?
   end
 end
