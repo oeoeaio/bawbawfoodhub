a5328692dcf58c2d4fbfa19659d169ac314aeeb7
Rob H <oeoeaio@gmail.com>
2020-03-13 09:39:11 +1100

    Revert "Revert "Use cms helper to link to job description file""
    This reverts commit af62c1d6d807ab201f90c05d2ed28863ccd3896d.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     # flash[:alert] = "We will be open up to and including Christmas Eve, then closed until Tue 14th of Jan." unless Date.today > Date.new(2020, 1, 13)
-    file_link = "https://bawbawfoodhub.org.au/rails/active_storage/disk/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9JYTJWNVNTSWRWR3RwZW1KalVFRm9TREUyV2xOMFZUUkVNVGhVWnprNEJqb0dSVlE2RUdScGMzQnZjMmwwYVc5dVNTSmJhVzVzYVc1bE95Qm1hV3hsYm1GdFpUMGlSMDFmYW05aVgyNWxkM05zWlhSMFpYSmZZV1F1Y0dSbUlqc2dabWxzWlc1aGJXVXFQVlZVUmkwNEp5ZEhUVjlxYjJKZmJtVjNjMnhsZEhSbGNsOWhaQzV3WkdZR093WlVPaEZqYjI1MFpXNTBYM1I1Y0dWSkloUmhjSEJzYVdOaGRHbHZiaTl3WkdZR093WlUiLCJleHAiOiIyMDIwLTAzLTEyVDIwOjU5OjQzLjcwMloiLCJwdXIiOiJibG9iX2tleSJ9fQ==--1268d7dfb19428abd6d104f0b1ce526e5cfbca8e/GM_job_newsletter_ad.pdf?content_type=application%2Fpdf&disposition=inline%3B+filename%3D%22GM_job_newsletter_ad.pdf%22%3B+filename%2A%3DUTF-8%27%27GM_job_newsletter_ad.pdf"
+    file_link = cms_fragment_render(:job_description, cms_page('general-manager'))
     flash[:alert] = %Q[We're hiring for the position of <strong><a href="#{file_link}">General Manager</a></strong> at the hub!].html_safe
   end
 
@@ -11,5 +11,12 @@ class HomeController < ApplicationController
 
   def questions
     @faq_groups = FaqGroup.preload(:faqs)
+  end
+
+  private
+
+  def cms_page(slug)
+    return unless @cms_site.present?
+    @cms_site.pages.published.find_by(slug: slug)
   end
 end
