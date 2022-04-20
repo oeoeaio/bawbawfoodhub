b18f2ee990718ec5b15d74a36b532d3ead2182c2
Rob H <oeoeaio@gmail.com>
2022-04-20 22:14:19 +1000

    Update configuration for rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -9,18 +9,17 @@ Bundler.require(*Rails.groups)
 module Bawbawfoodhub
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
     config.autoload_paths << Rails.root.join("lib")
     config.time_zone = "Melbourne"
 
     # Ensuring that ActiveStorage routes are loaded before Comfy's globbing
     # route. Without this file serving routes are inaccessible.
     config.railties_order = [ActiveStorage::Engine, :main_app, :all]
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
