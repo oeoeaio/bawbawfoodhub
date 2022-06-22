d17309f8ac68933c726d1404c8ecdc2b99cfc9bb
Rob H <oeoeaio@gmail.com>
2022-06-22 20:31:16 +1000

    require rails_helper instead of spec_helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe 'current_season' do
--- spec/mailers/subscription_mailer_spec.rb
+++ spec/mailers/subscription_mailer_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe SubscriptionMailer do
   describe 'confirmation' do
